9fe2d46fd6e738cd518556c588f23e5f1727acfb
ballicipluck <shyam4564@gmail.com>
2026-03-12 16:19:03 +0530

    add lazygit and tealdeer conf, update nvim options and keymaps, add copilot

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "x", '"_x', { desc = "Delete character without yanking" })
+vim.keymap.set("x", "x", '"_d', { desc = "Delete selection without yanking" })
+vim.keymap.set("n", "X", '"_diw', { desc = "Delete word without yanking" })
+vim.keymap.set("x", "X", '"_d', { desc = "Delete selection without yanking" })
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.conceallevel = 2
+vim.opt.clipboard = { "unnamed", "unnamedplus" }
+
 -- LSP Server to use for Rust.
 -- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
 -- only for diagnostics. The rest of LSP support will still be
